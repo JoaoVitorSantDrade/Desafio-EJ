2250c1e986589988203fe555645fa7c28cb91b5c
JoaoVitorSantDrade <jvitorsantdrade@gmail.com>
2022-04-27 13:10:00 -0300

    Atualizacao

---
+++ MyApp/resources/views/components/new-user.blade.php
@@ -0,0 +1,30 @@
+<div class="align-center">
+    <h2>Login</h2>
+</div>
+<form name="login" method="POST" action="/login">
+    @csrf
+    <section>
+        <div class="label-div">
+            <label for="nickname">Nome de usuário</label>
+            <input id="user_name" class="input-user"name="nickname" type="text" placeholder="Digite seu nome de usuário" required>
+            @error('nickname')
+            <p class="error">Nome de usuario já existe!</p>
+            @enderror
+        </div>
+        <div class="label-div">
+            <label for="password">Senha</label>
+            <input id="user_psw" class="input-psw" name="password" type="password" placeholder="Digite sua senha" required>
+            @error('password')
+            <p class="error">Senha com tamanho inválido!</p>
+            @enderror
+            <button id="visibility-icon" class="no-visibility visibility-icon" type="button"></button>
+            
+        </div>
+        <div>
+            <div class="label-div">
+                <button class="send-login" type="submit"></button>
+                <button class="new-user" type="button"></button>
+            </div>
+        </div>
+    </section>
+</form>
